0cb7c205089ae3cee1fb08beb94f562c2c389195
FrostKri <92458761+FrostKri@users.noreply.github.com>
2022-03-02 19:21:31 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, text }) => {
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + `【﻿ᴮᶜᴳᶜ】 ᵏʳⁱ𝘽𝙡𝙖𝙘𝙠𝙖𝙩 𝘽𝙤𝙩𝙯៚  【﻿ᴮᶜᴳᶜ】`), true).catch(_=>_)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + `【﻿ᴮᶜᴳᶜ】 𝙆𝙧𝙞𝙯𝙮𝙣 ×͜×  【﻿ᴮᶜᴳᶜ】`), true).catch(_=>_)
   m.reply('Selesai Broadcast All Group :)')
 }
 handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
